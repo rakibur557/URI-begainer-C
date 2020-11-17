b0fbbd77701f5fde3b3ef4c3585554a90cdbe069
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2020-11-18 01:49:40 +0600

    Add files via upload

---
+++ 1026.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    unsigned long int a,b,ans;
+    while (cin>>a>>b)
+    {
+       
+        ans=a^b;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
